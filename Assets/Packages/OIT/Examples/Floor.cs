9ebaaf468e83acc61e595350c690e80c8e5bef23
Nakata Nobuyuki <nobu.nakata@gmail.com>
2015-12-22 16:54:54 +0900

    no message

---
+++ Assets/Packages/OIT/Examples/Floor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace OIT {
+
+	public class Floor : MonoBehaviour {
+		public GameObject fab;
+		public Bounds field;
+		public int count;
+
+		void Start () {
+			for (var i = 0; i < count; i++) {
+				var pos = RandomPositionInField();
+				var go = (GameObject)Instantiate(fab, pos, Random.rotationUniform);
+				go.transform.SetParent(transform, true);
+			}
+		}
+		void OnDrawGizmos() {
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireCube(field.center, field.size);
+		}
+
+		Vector3 RandomPositionInField() {
+			var min = field.min;
+			var max = field.max;
+			return new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y), Random.Range(min.z, max.z));
+		}
+	}
+}
--- Assets/Packages/OIT/Shaders/OITRevealage.shader
+++ Assets/Packages/OIT/Shaders/OITRevealage.shader
@@ -18,23 +18,28 @@
 
 			struct appdata {
 				float4 vertex : POSITION;
+				float4 color : COLOR;
 				float2 uv : TEXCOORD0;
 			};
 			
 			struct vs2ps {
 				float4 vertex : POSITION;
+				float4 color : COLOR;
 				float2 uv : TEXCOORD0;			
+				float z : TEXCOORD1;
 			};
 			
 			vs2ps vert(appdata i) {
 				vs2ps o;
 				o.vertex = mul(UNITY_MATRIX_MVP, i.vertex);
 				o.uv = i.uv;
+				o.color = float4(i.color.rgb * i.color.a, i.color.a);
+				o.z = 0;
 				return o;
 			}
 			
 			float4 frag(vs2ps i) : COLOR {
-				 float4 c = tex2D(_MainTex, i.uv);
+				 float4 c = tex2D(_MainTex, i.uv) * i.color;
 				 return c.a;
 			}
 			ENDCG
